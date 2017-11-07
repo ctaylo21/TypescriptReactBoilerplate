38d5652a0336f8948720f277cbbd577b5e946fec
Caleb Taylor <caleb.taylor@nike.com>
2017-11-07 09:26:12 -0800

    added mocha-webpack options file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: './dist/bundle.js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/
-      }
-    ],
     rules: [
       {
         test: /\.js$/,
