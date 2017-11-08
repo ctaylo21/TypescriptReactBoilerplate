7e329ad64d203edaaea5e960f8872502b34d1ab2
Caleb Taylor <caleb.taylor@nike.com>
2017-11-08 12:59:40 -0800

    Create "npm run web" that allows mocha tests via browser

--- webpack/test-web.js
+++ webpack/test-web.js
@@ -1,11 +1,5 @@
 const path = require('path');
-
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   module: {
     rules: [
       {
