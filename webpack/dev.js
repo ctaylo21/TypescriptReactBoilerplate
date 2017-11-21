05b22d1ff32825268eabbc5a9bc4d0113a47ff89
Caleb Taylor <caleb.taylor@nike.com>
2017-11-21 16:24:29 -0600

    feat(webpack-dev-server): Added webpack server that supports hot-module-replacement reloading on changes

--- webpack/dev.js
+++ webpack/dev.js
@@ -2,13 +2,20 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./base.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, '../dist/'),
+    hot: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'ES6 modules and Webpack Starter Kit',
       template: path.resolve(__dirname, '../src/templates/index.html')
-    })
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 });
