8a8d9fb444b47c22139af3f3bc97ce5bf3e60781
Caleb Taylor <caleb.taylor@nike.com>
2017-11-21 17:13:13 -0600

    build(webpack): Optimized prod build by using recommended settings source-map and setting node env to "production"

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,9 +1,16 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./base.js');
 
 module.exports = merge(common, {
+  devtool: 'source-map',
   plugins: [
-    new UglifyJSPlugin()
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
   ]
 });
