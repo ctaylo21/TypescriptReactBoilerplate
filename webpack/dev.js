688a91d29b778a649e1c41c6c257e6f95aed589e
Caleb Taylor <caleb89taylor@gmail.com>
2018-04-11 11:23:26 -0700

    refactor(webpack): rely on default entry/output paths for webpack to reduce config

--- webpack/dev.js
+++ webpack/dev.js
@@ -5,11 +5,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = merge(common, {
-  entry: path.resolve(__dirname, '../src/app.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist')
-  },
   devServer: {
     contentBase: path.resolve(__dirname, '../dist/'),
     hot: true
--- webpack/prod.js
+++ webpack/prod.js
@@ -1,15 +1,9 @@
-const path = require('path')
 const merge = require('webpack-merge')
 const common = require('./base.js')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
-  entry: path.resolve(__dirname, '../src/app.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist')
-  },
   devtool: 'source-map'
   optimization: {
     minimizer: [
