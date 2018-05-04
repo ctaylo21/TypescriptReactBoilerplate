74d5aedfe9a1a36c64d29b3756abcd46365ae6ef
Caleb Taylor <caleb89taylor@gmail.com>
2018-05-04 14:14:06 -0700

    feat(webpack-serve): upgrade to webpack serve instead of webpack-dev-server

--- webpack/test.js
+++ webpack/test.js
@@ -10,8 +10,8 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, '../dist')
   },
   devtool: 'eval',
-  devServer: {
-    contentBase: [path.resolve(__dirname, '../dist/'), path.resolve(__dirname, '../node_modules/')]
+  serve: {
+    content: [path.resolve(__dirname, '../dist/'), path.resolve(__dirname, '../node_modules/')]
   },
   plugins: [
     new HtmlWebpackPlugin({
