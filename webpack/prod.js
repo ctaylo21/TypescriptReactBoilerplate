519af3c40f5a5dbc1c1bf73bd0e7f6228422a491
Caleb Taylor <caleb89taylor@gmail.com>
2018-06-03 08:47:09 -0700

    fix(CSS Minimizer): move CSS minimizer to plugin section so default JS minification works

--- webpack/prod.js
+++ webpack/prod.js
@@ -5,9 +5,17 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  optimization: {
-    minimizer: [
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  }
+  plugins: [
+    new OptimizeCSSAssetsPlugin({
+      cssProcessorOptions: {
+        map: {
+          inline: false
+        },
+        discardComments: {
+          removeAll: true
+        }
+      },
+      canPrint: true
+    })
+  ]
 })
