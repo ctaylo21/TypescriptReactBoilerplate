580c048439d65182fe3fd40829e9bbff21f87a84
Caleb Taylor <caleb89taylor@gmail.com>
2018-06-03 09:29:48 -0700

    feat(await): add support for async/await via babel plugin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import {sum, product} from './modules/math'
+import 'regenerator-runtime/runtime'
+import {sum, product, doubleAfterDelay} from './modules/math'
 import './styles/app.scss'
 
 const resultSum = document.getElementById('resultSum')
 const resultProduct = document.getElementById('resultProduct')
+const resultAwaitSum = document.getElementById('resultAwaitSum')
 
 const a = 3
 const b = 7
 
 resultSum.textContent = `The sum of ${a} and ${b} is ${sum(a, b)}. ✨`
 resultProduct.textContent = `The product of ${a} and ${b} is ${product(a, b)}. 🚀`
+
+async function addAsync (x) {
+  const a = await doubleAfterDelay(10)
+  const b = await doubleAfterDelay(20)
+  const c = await doubleAfterDelay(30)
+  return x + a + b + c
+}
+
+addAsync(5).then((sum) => {
+  resultAwaitSum.textContent = `The delayed sum of 5 and doubling 10, 20, and 30 is ${sum}`
+})
--- src/modules/math.js
+++ src/modules/math.js
@@ -5,3 +5,11 @@ export function sum (a, b) {
 export function product (a, b) {
   return a * b
 }
+
+export function doubleAfterDelay (x, delay = 2000) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x * 2)
+    }, delay)
+  })
+}
